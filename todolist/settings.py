68624db7cd28b7e6e2546bffd841205eb8fd14e7
Ahmadou Bamba Diagne <diagne.bambahmadou@gmail.com>
2022-05-14 12:57:44 +0000

    Add models

--- todolist/settings.py
+++ todolist/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'todolist_functions_views.apps.TodolistFunctionsViewsConfig',
+    "users.apps.UsersConfig"
 ]
 
 MIDDLEWARE = [
--- todolist_functions_views/models.py
+++ todolist_functions_views/models.py
@@ -1,3 +1,14 @@
+from datetime import datetime
 from django.db import models
 
-# Create your models here.
+class SimpleUser(models.Model):
+    name=models.CharField(max_length=50)
+    password=models.CharField(max_length=70)
+
+class Todo(models.Model):
+    user = models.ForeignKey(SimpleUser, on_delete=models.CASCADE)
+    task=models.CharField(max_length=40,blank=False)
+    STATUS_TASK = (("TO_DO", "TO_DO"),("IN_PROGRESS","IN_PROGRESS") ,("FINISHED", "FINISHED"))
+    status = models.CharField(max_length=30, choices=STATUS_TASK,blank=False,default="TO_DO")
+    created_at=models.DateTimeField (default=datetime.now, blank = True)
+    
